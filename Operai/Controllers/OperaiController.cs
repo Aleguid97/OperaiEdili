d50172679b262803b61ea4efdb5e27f1415c8fb5
Aleguid97 <aleguid97@gmail.com>
2024-02-26 16:40:51 +0100

    Aggiungere i file di progetto.

---
+++ Operai/Controllers/OperaiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Operai.Models; // Don't forget to include the namespace of your Operai class
+
+namespace Operai.Controllers
+{
+    public class OperaiController : Controller
+    {
+        // GET: Operai
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ Operai/Models/Oggetti.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Operai.Models
+{
+    public class Operai
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string Mansione { get; set; }
+        public bool Sposato { get; set; }
+        public int Figli { get; set; }
+
+       
+        public Operai(string nome, string cognome, string indirizzo, string codiceFiscale, string mansione, bool sposato, int figli)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Indirizzo = indirizzo;
+            CodiceFiscale = codiceFiscale;
+            Mansione = mansione;
+            Sposato = sposato;
+            Figli = figli;
+        }
+    }
+}
